dd04c1b2fd2c2dd54cfbdfbeb2513b85c96f755f
GrossWhiteRabbit <vowa.kononenko@gmail.com>
2021-02-13 17:55:19 +0200

    Updates

---
+++ precache-manifest.53d34e8dae3b713b3634094eec964db4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4a3a9b77bc02dac6c2a9afdd12409d0",
+    "url": "/react_redux_restoran/index.html"
+  },
+  {
+    "revision": "053f3f560d91d79f7460",
+    "url": "/react_redux_restoran/static/css/main.482f25ec.chunk.css"
+  },
+  {
+    "revision": "eff762f1efb6b8b11dac",
+    "url": "/react_redux_restoran/static/js/2.da0404ce.chunk.js"
+  },
+  {
+    "revision": "053f3f560d91d79f7460",
+    "url": "/react_redux_restoran/static/js/main.d2d34454.chunk.js"
+  },
+  {
+    "revision": "d79fb889299e025c530a",
+    "url": "/react_redux_restoran/static/js/runtime~main.fcd56f69.js"
+  },
+  {
+    "revision": "698e5e807daf0026f6ac590ce288822c",
+    "url": "/react_redux_restoran/static/media/food-bg.698e5e80.jpg"
+  },
+  {
+    "revision": "701d6d1d57f5d528c4db1e887ff29e82",
+    "url": "/react_redux_restoran/static/media/shopping-cart-solid.701d6d1d.svg"
+  }
+]);
